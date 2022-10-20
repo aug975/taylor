eebbc313c9903bf1291bb577b85261d4b28b265d
August <101229028+aug975@users.noreply.github.com>
2022-10-20 14:52:39 -0300

    upload da main sem paralelismo
    o código agora foi divido em dois, separando as versões com e sem paralelismo

---
+++ TaylorSemParalelismo.java
@@ -0,0 +1,44 @@
+//VERSAO DE CODIGO SEM USO DE PARALELISMO
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.BigInteger;
+
+@SuppressWarnings("unused")
+class Main {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println("\n\nResult: "+getExp(1,10000,100000));
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.printf("\nTempo sem paralelismo: %.3f s%n", (elapsed) / 1000d);
+    }
+    
+    @SuppressWarnings("deprecation")
+	private static BigDecimal getExp(double x, int precision, int steps){
+         BigDecimal sum = BigDecimal.ZERO;
+         BigDecimal X = BigDecimal.valueOf(x);
+         BigDecimal m;
+         for(int i=0; i<steps; i++){      
+             m = X.pow(i).divide(new BigDecimal(Main.factorial(i)),precision,BigDecimal.ROUND_HALF_UP);
+             //System.out.println("\nx^"+i+"/"+i+"! = "+m);
+             sum = sum.add(m);
+
+             // quando incremento da serie menor do que precisao interrompe a soma
+             if (BigDecimal.ZERO.compareTo(m) == 0){
+               return sum;
+             } 
+         }
+         return sum;
+    }
+    
+    private static BigInteger factorial(int n){
+        BigInteger f = BigInteger.ONE;
+        if(n!=0){
+            for(int i=1; i<=n; i++){
+                f = f.multiply(BigInteger.valueOf(i));
+            }
+        }
+        return f;
+    }
+}
