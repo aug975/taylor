e3264e3c989e9e98e3e8a0df5836981439ca1fa0
August <101229028+aug975@users.noreply.github.com>
2022-10-20 14:51:19 -0300

    performance aumentada
    -performance foi aumentada removendo-se computação redudante (pós convergência, retornava apenas zeros) 
    -tarefas foram distribuídas de forma melhor (as primeiras eram mais rápidas e as últimas mais lentas, agora o esforço é mais homogêneo)

---
+++ mainComParalelismo.java
@@ -0,0 +1,114 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.BigInteger;
+
+class Tarefa extends Thread {
+
+    private double x;
+    private int precision;
+    private int interval;
+    private int steps;
+    private BigDecimal sum = BigDecimal.ZERO;
+
+    private static BigInteger factorial(int n){  
+      BigInteger f = BigInteger.ONE;
+      if(n!=0){
+        for(int i=1; i<=n; i++){
+          f = f.multiply(BigInteger.valueOf(i));
+        }
+      }
+    return f;
+    }
+  
+    //método construtor que receberá os parâmetros da tarefa
+    public Tarefa(double x, int precision, int interval, int steps) {
+        this.x = x;
+        this.precision = precision;
+        this.interval = interval;
+        this.steps = steps;
+          
+    }
+
+    //método que retorna o total calculado
+    public BigDecimal getTotal() {
+        return sum;
+    }
+
+    /*
+     Este método se faz necessário para que possamos dar start() na Thread
+     e iniciar a tarefa em paralelo
+     */
+	@Override
+    public void run() {
+      BigDecimal X = BigDecimal.valueOf(x);
+      BigDecimal m;
+      int termo;
+         
+      for(int i=0; i<=interval; i++){ 
+        termo = (i*4)+steps-1;
+        m = X.pow(termo).divide(new BigDecimal(Tarefa.factorial(termo)),precision,BigDecimal.ROUND_HALF_UP);   
+        // quando incremento da serie menor do que precisao interrompe a soma
+        if (BigDecimal.ZERO.compareTo(m) == 0){
+          return;
+        }  
+        //print apenas para acompanhar execucao
+        //System.out.println(".");
+       sum = sum.add(m);
+      
+      }
+      return;
+
+    }
+}
+
+class Main {
+    
+    public static void main(String[] args) {
+
+      int precision = 10000;
+      int T = 100000;
+
+      long start = System.currentTimeMillis();
+      //cria quatro tarefas
+      Tarefa t1 = new Tarefa(1, precision, T/4, 1);
+      t1.setName("Tarefa1");
+      Tarefa t2 = new Tarefa(1, precision, T/4, 2);
+      t2.setName("Tarefa2");
+      Tarefa t3 = new Tarefa(1, precision, T/4, 3);
+      t3.setName("Tarefa3");
+      Tarefa t4 = new Tarefa(1, precision, T/4, 4);
+      t4.setName("Tarefa4");
+
+      BigDecimal tot = BigDecimal.ZERO;
+
+      //inicia a execução paralela das quatro tarefas, iniciando quatro novas threads no programa
+      t1.start();
+      t2.start();
+      t3.start();
+      t4.start();
+
+      //aguarda a finalização das tarefas
+      //faz o somatório dos totalizadores de cada Thread
+      try {
+        t1.join();
+        tot = tot.add(t1.getTotal());
+        t2.join();
+        tot = tot.add(t2.getTotal());
+        t3.join();
+        tot = tot.add(t3.getTotal());
+        t4.join();
+        tot = tot.add(t4.getTotal());
+        } 
+      catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+      System.out.println("Total: " + tot);
+      
+      long elapsed = System.currentTimeMillis() - start;
+      System.out.printf("\nTempo com paralelismo: %.3f s%n", (elapsed) / 1000d);
+
+            
+    }
+
+}
